45fdeb8030c598830149c40cb36577a748b47427
Jim <jimmycn03@gmail.com>
2024-04-28 18:25:45 +0300

    Commenting the code

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,16 @@
 -- Sets up a mysql server
+
+-- The database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' identified BY '@Nyongi03';
+
+-- A new user with password
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' identified BY 'hbnb_dev_pwd';
+
+-- Granting user privileges
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Granting user performance schema
 GRANT select on performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- flushing privileges to function
 FLUSH PRIVILEGES;
