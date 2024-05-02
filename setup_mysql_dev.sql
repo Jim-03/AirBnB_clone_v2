3772b877189ec9a49f9191568363144152863c33
Jim <jimmycn03@gmail.com>
2024-05-02 08:42:46 +0300

    Correcting the casing to uppercase for mysql commands

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -10,7 +10,7 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' identified BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 -- Granting user performance schema
-GRANT select on performance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT on performance_schema.* TO 'hbnb_dev'@'localhost';
 
 -- flushing privileges to function
 FLUSH PRIVILEGES;
