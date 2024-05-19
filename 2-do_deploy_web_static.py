49b693008303e718646a8158de3266187a312149
Jim <jimmycn03@gmail.com>
2024-05-19 17:02:12 +0300

    distributes an archive across web servers

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Distributes an archive across web servers."""
+from fabric.api import *
+import os
+
+
+env.hosts = ['52.3.246.175', '54.82.207.91']
+def do_deploy(archive_path):
+    """Distributes the archive."""
+    if not os.path.exists(archive_path):
+        return False
+
+    filename = os.path.basename(archive_path)
+    rel = filename.split('.')[0]
+    folder = f"/data/web_static/releases/{rel}"
+
+    try:
+        put(archive_path, f"/tmp/{filename}")
+        run(f"sudo mkdir -p {folder}")
+        run(f"sudo tar -xzf /tmp/{filename} -C {folder}")
+        run(f"sudo rm /tmp/{filename}")
+        run(f"sudo mv {folder}/web_static/* {folder}/")
+        run(f"sudo rm -rf {folder}/web_static")
+        run(f"sudo rm -rf /data/web_static/current")
+        run(f"sudo ln -s {folder} /data/web_static/current")
+        return True
+    except:
+        return False
