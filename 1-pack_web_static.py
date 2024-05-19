723a444ddcbfe8568b8d352ba6627310f231a86c
Jim <jimmycn03@gmail.com>
2024-05-19 16:34:25 +0300

    generates a .tgz archive

---
+++ 1-pack_web_static.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Generates a .tgz archive."""
+from fabric.api import *
+from datetime import datetime
+
+
+def do_pack():
+    """Generates the .tgz archive from web static."""
+    local("sudo mkdir -p versions")
+    now =datetime.now().strftime("%Y%m%d%H%M%S")
+    name = f"versions/web_static_{now}.tgz"
+    ans = local(f"sudo tar -cvzf {name} web_static")
+
+    if ans.succeeded:
+        return name
+    else:
+        return None
